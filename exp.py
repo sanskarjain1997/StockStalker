f9dde3c4408cdb1c252dbc7c30f65b281803d2b4
sanskarjain1997 <sanskarjain1997.19@gmail.com>
2019-03-09 21:59:17 +0530

    Intragrating Proprocessing part to main program to pass data to model

--- exp.py
+++ exp.py
@@ -1,15 +1,15 @@
 from get_pure_data import Data
 from model import build_model
 
+window = 25
+
 tcs_data = Data('TCS')
 tcs_data.preprocess()
-x_train, y_train, x_test, y_test = tcs_data.split_data(window=25)
+x_train, y_train, x_test, y_test = tcs_data.split_data(window=window)
 
-#Preprocessing
-window = 45 #
-model = build_model()
-#Splitting the Data
-x_train, y_train, x_test, y_test = split_data(data)
+model = build_model([x_train.shape[2], window, 100, 1])
+model.fit(x_train, y_train, batch_size= , epochs= , verbose= )
 #Predicting
+predictions = model.predict(x_test)
 #Trading
 #Plotting and printing results
